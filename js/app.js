81e4a4042c24dd9dc551735ac7bb95541d1d441b
Andy Agulue <andyfandandy@gmail.com>
2020-11-16 17:17:19 -0800

    changed questions on javascript

---
+++ js/app.js
@@ -0,0 +1,17 @@
+'use strict'
+let answer = prompt ('Do you have a dog in your life?');
+
+let name = prompt ('What is your dogs name');
+
+let breed = prompt ('What breed is your dog?');
+
+
+
+alert('Hi ' + name + ' the ' + breed);
+
+
+
+
+
+
+
